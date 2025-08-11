164d36262272376c2c6f35a297e1704d11cd3232
Aynal <aynalhaque7411@gmail.com>
2025-08-11 11:11:54 +0600

    deploy stage

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn milon_agency.wsgi:application
--- milon_agency/settings.py
+++ milon_agency/settings.py
@@ -29,7 +29,8 @@ if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['our-agency.onrender.com', 'localhost']
+
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
